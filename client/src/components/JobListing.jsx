ea6745d24464de507d504b6e9aa87054c886c867
jablo <jablo@decipher.decipher>
2025-08-09 02:44:50 +0300

    Feat: Updating Navbar to suit all screens

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -7,6 +7,8 @@ const JobListing = () => {
 
   const {isSearched, searchFilter, setSearchFilter, jobs} = React.useContext(AppContext)
 
+  const [showFilter, setShowFilter] = React.useState(false)
+
   return (
     <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
       {/* Sidebar */}
@@ -34,8 +36,12 @@ const JobListing = () => {
           )
         }
 
+        <button onClick={ e => setShowFilter(prev => !prev) } className='px-6 py-1.5 rounded border border-gray-400 lg:hidden'>
+          {showFilter ? "Close" : "Filters"}
+        </button>
+
         {/* Category Filter */}
-        <div className='max-lg:hidden'>
+        <div className={showFilter ? "" : "max-lg:hidden"}>
           <h4 className='font-medium text-lg py-4'>Search by Categories</h4>
           <ul className='space-y-4 text-gray-600'>
             {
@@ -49,7 +55,7 @@ const JobListing = () => {
           </ul>
         </div>
         {/* Location Filter */}
-        <div className='max-lg:hidden'>
+        <div className={showFilter ? "" : "max-lg:hidden"}>
           <h4 className='font-medium text-lg py-4 pt-14'>Search by Location</h4>
           <ul className='space-y-4 text-gray-600'>
             {
