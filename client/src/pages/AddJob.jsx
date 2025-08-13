f6f0e8f8ffffe6b27868b0f8cc965e86996a13aa
jablo <jablo@decipher.decipher>
2025-08-13 03:04:17 +0300

    Updated addjobjsx file
    edited set functions of addjobjsx file

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const AddJob = () => {
+
+  const [title, setTitle] = React.useState('');
+  const [location, setLocation] = React.useState('Bangalore');
+  const [category, setCategory] = React.useState('Programming');
+  const [level, setLevel] = React.useState('Beginner level');
+  const [salary, setSalary] = React.useState(0);
+
   return (
     <div>
+
     </div>
   )
 }
