a1e3e8a694eae19503ff50c9c2275ef458229ff6
jablo <jablo@decipher.decipher>
2025-08-14 22:33:42 +0300

    created company controller
    added company controller in controllers folder

---
+++ server/controllers/companyController.js
@@ -0,0 +1,23 @@
+// register a company
+export const registerCompany = async (req, res) => {}
+
+// login a company
+export const loginCompany = async (req, res) => {}
+
+// get company data
+export const getCompanyData = async (req, res) => {}
+
+// post a new job
+export const postJob = async (req, res) => {}
+
+// get company job applicants
+export const getCompanyJobApplicants = async (req, res) => {}
+
+// get posted jobs by a company
+export const getCompanyPostedJobs = async (req, res) => {}
+
+// change job applications status
+export const changeJobApplicationsStatus = async (req, res) => {}
+
+// change job visibility
+export const changeVisibility = async (req, res) => {}
