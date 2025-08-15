d3a892cda4e85b3f6c8e47e3954ffcfc36bb58df
jablo <jablo@decipher.decipher>
2025-08-15 15:23:56 +0300

    Feat: Created userRoutes and userController
    added userroutes and updated serverjs file, also added usercontroller
    for handling logics

---
+++ server/controllers/userController.js
@@ -0,0 +1,12 @@
+// get user data
+export const getUserData = async (req, res) => {}
+
+// apply for job
+export const applyForJob = async (req, res) => {}
+
+// get user applied applications
+export const getUserJobApplications = async (req, res) => {}
+
+// update user profile(resume)
+export const updateUserResume = async (req, res) => {}
+
