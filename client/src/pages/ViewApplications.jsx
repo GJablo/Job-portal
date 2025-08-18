950e11b966ab0ae4af5775a9da1f0e568b5368b3
jablo <jablo@decipher.decipher>
2025-08-19 01:28:09 +0300

    UI Fixes

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -7,15 +7,15 @@ import Loading from '../components/Loading'
 
 const ViewApplications = () => {
 
-  const {backendUrl, companyToken} = React.useContext(AppContext)
+  const { backendUrl, companyToken } = React.useContext(AppContext)
   const [applications, setApplications] = React.useState(false)
 
   // function to fetch company job applicants
 
   const fetchCompanyJobApplicants = async () => {
     try {
-      const {data} = await axios.get(`${backendUrl}/api/company/applicants`, {
-        headers: {token: companyToken, 'Cache-Control': 'no-cache', 'Pragma': 'no-cache'}, params: { t: new Date().getTime() }
+      const { data } = await axios.get(`${backendUrl}/api/company/applicants`, {
+        headers: { token: companyToken, 'Cache-Control': 'no-cache', 'Pragma': 'no-cache' }, params: { t: new Date().getTime() }
       })
       if (data.success) {
         setApplications(data.applications.reverse())
@@ -31,10 +31,10 @@ const ViewApplications = () => {
 
   const changeJobApplicationStatus = async (id, status) => {
     try {
-      const {data} = await axios.post(`${backendUrl}/api/company/change-status`, {
+      const { data } = await axios.post(`${backendUrl}/api/company/change-status`, {
         id, status
       }, {
-        headers: {token: companyToken}
+        headers: { token: companyToken }
       })
       if (data.success) {
         toast.success('Status updated successfully')
@@ -52,24 +52,30 @@ const ViewApplications = () => {
     fetchCompanyJobApplicants()
   }, [backendUrl, companyToken])
 
-  return applications ? applications.length === 0 ? ( <div></div> ) : (
+  return applications ? applications.length === 0 ? (
+
+    <div className='flex items-center justify-center min-h-[70vh]'>
+      <p className='text-xl sm:text-2xl'>No applications available</p>
+    </div>
+
+  ) : (
     <div className='container mx-auto p-4'>
       <div>
         <table className='w-full max-w-4xl bg-white border border-gray-200 max-sm:text-sm'>
           <thead>
-          <tr className='border-b'>
-            <th className='py-2 px-4 text-left'>#</th>
-            <th className='py-2 px-4 text-left'>User name</th>
-            <th className='py-2 px-4 text-left max-sm:hidden'>Job Title</th> 
-            <th className='py-2 px-4 text-left max-sm:hidden'>Location</th>
-            <th className='py-2 px-4 text-left'>Resume</th>
-            <th className='py-2 px-4 text-left'>Action</th>
-          </tr>
+            <tr className='border-b'>
+              <th className='py-2 px-4 text-left'>#</th>
+              <th className='py-2 px-4 text-left'>User name</th>
+              <th className='py-2 px-4 text-left max-sm:hidden'>Job Title</th>
+              <th className='py-2 px-4 text-left max-sm:hidden'>Location</th>
+              <th className='py-2 px-4 text-left'>Resume</th>
+              <th className='py-2 px-4 text-left'>Action</th>
+            </tr>
           </thead>
           <tbody>
             {applications.filter(item => item.jobId && item.userId).map((applicant, index) => (
               <tr key={index} className='text-gray-700'>
-                <td className='py-2 px-4 border-b text-center'>{index+1}</td>
+                <td className='py-2 px-4 border-b text-center'>{index + 1}</td>
                 <td className='py-2 px-4 border-b text-center flex items-center'>
                   <img className='w-10 h-10 rounded-full mr-3 max-sm:hidden' src={applicant.userId.image} alt="" />
                   <span>{applicant.userId.name}</span>
@@ -84,15 +90,15 @@ const ViewApplications = () => {
                   </a>
                 </td>
                 <td className='py-2 px-4 border-b relative'>
-                  { applicant.status === 'pending' ?
-                  <div className='relative inline-block text-left group'>
-                    <button className='text-gray-500 action-button'>...</button>
-                    <div className='z-10 hidden absolute right-0 md:left-0 top-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow group-hover:block'>
-                      <button onClick={() => changeJobApplicationStatus(applicant._id, 'Accepted')} className='block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100'>Accept</button>
-                      <button onClick={()=> changeJobApplicationStatus(applicant._id, 'Rejected')} className='block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100'>Reject</button>
+                  {applicant.status === 'pending' ?
+                    <div className='relative inline-block text-left group'>
+                      <button className='text-gray-500 action-button'>...</button>
+                      <div className='z-10 hidden absolute right-0 md:left-0 top-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow group-hover:block'>
+                        <button onClick={() => changeJobApplicationStatus(applicant._id, 'Accepted')} className='block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100'>Accept</button>
+                        <button onClick={() => changeJobApplicationStatus(applicant._id, 'Rejected')} className='block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100'>Reject</button>
+                      </div>
                     </div>
-                  </div>
-                  : <span className={`px-3 py-1 rounded ${applicant.status === 'Accepted' ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'}`}>{applicant.status}</span>}
+                    : <span className={`px-3 py-1 rounded ${applicant.status === 'Accepted' ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'}`}>{applicant.status}</span>}
                 </td>
               </tr>
             ))}
@@ -101,7 +107,7 @@ const ViewApplications = () => {
       </div>
     </div>
   ) : (
-  <Loading />
+    <Loading />
   )
 }
 
